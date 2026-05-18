e126d48b2682f93f0dd68bd8d1cf62234fe49f75
Abdul_Razzaq <abdulrzq.dev@gmail.com>
2026-05-18 11:20:19 +0500

    Uncommented Twilio configuration

--- call_test.py
+++ call_test.py
@@ -1,7 +1,7 @@
 import requests
 
 # Aapka Data
-#account_sid = "AC7790b9f52f3604313f889c17dfc9497e"
+account_sid = "AC7790b9f52f3604313f889c17dfc9497e"
 auth_token = "0514121108d1f63b318225a5c6c35cfb"
 to_number = "+923238292357"
 from_number = "+18142921639"
