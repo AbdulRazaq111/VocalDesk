d136d6c03aa4a31b05c5697317fb4d839b6bce6d
Abdul_Razzaq <abdulrzq.dev@gmail.com>
2026-06-11 21:09:40 +0500

    build: force clear render structural pipe deadlock

--- main.py
+++ main.py
@@ -468,4 +468,5 @@ async def voice_endpoint():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000 )
+
